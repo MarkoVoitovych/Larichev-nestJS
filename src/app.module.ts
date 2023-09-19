45af8bd8c0758c28638cb0f360241e17dbe61afe
MarkoVoitovych <searchangeos@gmail.com>
2023-09-19 12:08:31 +0300

    feat: add test userModel

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,18 @@ import { PageModule } from './page/page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
 	imports: [
+		MongooseModule.forRoot('mongodb://localhost/test'),
 		ConfigModule.forRoot(),
 		AuthModule,
 		PageModule,
 		ProductModule,
 		ReviewModule,
+		UsersModule,
 	],
 	controllers: [AppController],
 	providers: [AppService],
